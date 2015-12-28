143b9f4c6b0ea58eaf5df66db9017d52720d6982
Shawn Paul Smith <sycomonkey@gmail.com>
2015-12-27 23:39:56 -0800

    Add github link, fix script for dev

--- wfn.sh
+++ wfn.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
-# TODO: Set it up so that this process dies if either child does.
-ls
-if [[ -x "/go/bin/wfn" ]]; then
-    echo "Starting wfn backend..."
-    /go/bin/wfn &
-else
-    echo "Could not find /go/bin/wfn"
-fi
-
-echo "In between"
-
-if [[ -x "/go/bin/caddy" ]]; then
-    echo "Starting caddy frontend..."
-    /go/bin/caddy -root ./root
-else
-    echo "Could not find /go/bin/caddy"
-fi
+echo "Starting wfn backend..."
+/go/bin/wfn &
+echo "Starting caddy frontend..."
+/go/bin/caddy -root ./root
